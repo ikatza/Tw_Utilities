ff7daf56e334cc6a9266c9c0367ee5087d084f2f
Iker de Icaza <icasik@ciencias.unam.mx>
2016-05-11 13:42:16 -0500

    Added the follow_users() function, it has some conditions already working out properly.

--- testing_utilities.py
+++ testing_utilities.py
@@ -11,12 +11,21 @@ import datetime
 import unidecode
 import unicodedata
 
+
 def get_tokens():
     tokens_file = open('Tokens.json')
     tokens_json = tokens_file.read()
     tokens_file.close()
     tokens_list = json.loads(tokens_json)
     return tokens_list
+
+
+def open_json(file_name):
+    filee = open(file_name)
+    jsonn = filee.read()
+    filee.close()
+    json_list = json.loads(jsonn)
+    return json_list
 
 
 def create_tweepy_object(tokens):
@@ -44,8 +53,17 @@ def create_tweepy_object(tokens):
 #     return response
 
 
-def get_account_followers(scrname_to_stalk):
-    tokens_list = get_tokens()
+def give_follow(usr):
+    if usr['followed_by'] or usr['following']:
+        return False
+    elif usr['omit']:
+        return False
+    else:
+        return True
+
+
+def get_account_followers_and_initialize_data(scrname_to_stalk):
+    tokens_list = open_json('Tokens.json')
     tweepy_api = create_tweepy_object(tokens_list[0])
     # user = tweepy_api.get_user(screen_name=screen_name)
     try:
@@ -58,7 +76,7 @@ def get_account_followers(scrname_to_stalk):
 
     print "I'll get ", scrname_to_stalk, " followers list."
     user_to_stalk = tweepy_api.get_user(screen_name=scrname_to_stalk)
-    print "He/She has "+str(user_to_stalk.followers_count)+ " followers."
+    print "He/She has " + str(user_to_stalk.followers_count) + " followers."
     # print user.friends_count
 
     followers_list = []
@@ -68,24 +86,20 @@ def get_account_followers(scrname_to_stalk):
         for u in page:
             usr['screen_name'] = u.screen_name
             usr['name'] = u.name
-            usr['data_retrieved'] = str(datetime.date.today())
+            usr['date_retrieved'] = str(datetime.date.today())
+            usr['date_followed'] = ''
 
-            ### After to compare:
-            # format = "%Y-%m-%d"
-            # s = data_retrieved
-            # d = datetime.datetime.strptime(s, format) + datetime.timedelta(days=10)
-            # if d.date() < datetime.date.today():
-            #     unfollow()
-
-
-
-            friendship = tweepy_api.show_friendship(source_screen_name=me.screen_name, target_screen_name=u.screen_name)
+            friendship = tweepy_api.show_friendship(
+                source_screen_name=me.screen_name,
+                target_screen_name=u.screen_name)
             usr['followed_by'] = friendship[0].followed_by
             usr['following'] = friendship[0].following
+            usr['omit'] = False
 
             followers_list.append(usr.copy())
 
     directory = 'Data_'+scrname_to_stalk
+    file_name = directory+'/'+scrname_to_stalk+'_followers.json'
     try:
         os.makedirs(directory)
     except OSError:
@@ -97,13 +111,77 @@ def get_account_followers(scrname_to_stalk):
         print "Reported followers: ", str(user_to_stalk.followers_count)
         print "Retrieved followers: ", str(len(followers_list))
 
-    print 'Saving followers on '+directory+'/'+scrname_to_stalk+'_followers.json'
-    with open(directory+'/'+scrname_to_stalk+'_followers.json', 'w+') as outfile:
+    print 'Saving followers on ' + file_name
+    with open(file_name, 'w+') as outfile:
         json.dump(followers_list, outfile, ensure_ascii=False)
 
 
+def follow_users(scrname_to_stalk, follows_limit):
+    tokens_list = open_json('Tokens.json')
+    tweepy_api = create_tweepy_object(tokens_list[0])
+    # user = tweepy_api.get_user(screen_name=screen_name)
+    try:
+        me = tweepy_api.verify_credentials()
+    except False:
+        print "Wrong credentials"
+        exit()
+    print "I'm ", me.name, "(", me.screen_name, ")"
+    # print tweepy_api.me()
+
+    print "I'll follow ", scrname_to_stalk, " followers."
+    # user_to_stalk = tweepy_api.get_user(screen_name=scrname_to_stalk)
+
+    directory = 'Data_'+scrname_to_stalk
+    file_name = directory+'/'+scrname_to_stalk+'_followers.json'
+    users_list = open_json(file_name)
+    follow_count = 0
+    for usr in users_list:
+        if give_follow(usr):
+            try:
+                tweepy_api.create_friendship(usr['screen_name'])
+                print "Just gave ", usr['screen_name'], "(", usr['name'], ") follow."
+                follow_count += 1
+                usr['following'] = True
+                usr['date_followed'] = str(datetime.date.today())
+            except tweepy.error.TweepError as e:
+                print e
+                usr['omit'] = True
+        if follow_count == follows_limit:
+            break
+    print "Updating the followers file."
+    with open(file_name, 'w+') as outfile:
+        json.dump(users_list, outfile, ensure_ascii=False)
+
+
+def unfollow_users():
+    tokens_list = open_json('Tokens.json')
+    tweepy_api = create_tweepy_object(tokens_list[0])
+    # user = tweepy_api.get_user(screen_name=screen_name)
+    try:
+        me = tweepy_api.verify_credentials()
+    except False:
+        print "Wrong credentials"
+        exit()
+    print "I'm ", me.name, "(", me.screen_name, ")"
+    # print tweepy_api.me()
+
+    print "I'll unfollow ", scrname_to_stalk, " followers that haven't follow me back =|."
+    user_to_stalk = tweepy_api.get_user(screen_name=scrname_to_stalk)
+
+
+
+            ### After to compare:
+            # format = "%Y-%m-%d"
+            # s = data_retrieved
+            # d = datetime.datetime.strptime(s, format) + datetime.timedelta(days=10)
+            # if d.date() < datetime.date.today():
+            #     unfollow()
+
+
+
+
 def get_follws_and_befriend_them(screen_name):
-    tokens_list = get_tokens()
+    tokens_list = open_json('Tokens.json')
     tweepy_api = create_tweepy_object(tokens_list[0])
     # user = tweepy_api.get_user(screen_name=screen_name)
     try:
@@ -145,8 +223,8 @@ if __name__ == '__main__':
     # screen_name_to_stalk = "Cintli"
     # get_follws_and_befriend_them(screen_name_to_stalk)
 
-    tokens_list = get_tokens()
-    tweepy_api = create_tweepy_object(tokens_list[0])
+    # tokens_list = open_json('Tokens.json')
+    # tweepy_api = create_tweepy_object(tokens_list[0])
     # screen_name = 'babrielpalacios'
     # screen_name = 'gabyinthewild'
     # try:
@@ -158,4 +236,5 @@ if __name__ == '__main__':
     # print "Out and ok"
 
     screen_name_to_stalk = "DiegoCorro1"
-    get_account_followers(screen_name_to_stalk)
+    # get_account_followers_and_initialize_data(screen_name_to_stalk)
+    follow_users(screen_name_to_stalk, 2)
